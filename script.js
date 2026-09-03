1757b44845b7a52aa52dfc3709f6002986837d1b
LilwuyenakaKinggMeow <trannguyen72726@gmail.com>
2026-09-03 11:30:03 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    alert(`Thank you, ${name}! Your message has been received.`);
+
+    contactForm.reset();
+});
